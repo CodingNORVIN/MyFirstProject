06b47430e726e5a96104e5a908ec9a972ffe083a
nor1vin <nor1vin.code@gmail.com>
2025-08-05 22:52:08 +0100

    This is my first ever java program Day 1 22:51  05/08/2025

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+   public static void main (String [] args){
+
+
+       //THIS IS MY FIRST JAVA PROGRAM
+
+
+
+       /*
+        THIS
+        IS
+        A
+        MULTI
+        LINE
+        COMMENT
+        */
+
+
+
+        System.out.print("This is my first JAVA program\n");
+        System.out.print("It is really cool\n");
+        System.out.print("Learning Java Day 1");
+   }
+}
